0dbd807734695843821d839900f40e8c6adb3afd
NykylayMaximov <128116021+NykylayMaximov@users.noreply.github.com>
2023-06-23 11:30:34 +0900

    working

--- news/views.py
+++ news/views.py
@@ -38,7 +38,7 @@ class PostDateil(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['is_not_subscribe'] = self.request.user.subscribers.filter(category='Наука')
+        context['is_not_subscribe'] = self.request.user.subscribers.get(category='Спорт')
         return context
 
 
